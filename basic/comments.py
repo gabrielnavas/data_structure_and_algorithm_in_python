bd1f275b543aa0db5ad3cea888026a28c1429d34
Navas <gabrielnavas@email.com>
2021-03-17 17:50:13 -0300

    basics: comments

---
+++ basic/comments.py
@@ -0,0 +1,17 @@
+print('hello word')
+"I'm a one line comment \o/"
+print('start comments')
+# print('Im a one line comment')
+
+""" 
+	I am a multiple line comments
+	I am a multiple line comments
+	I am a multiple line comments
+"""
+
+'''
+	I am a multiple line comments
+	I am a multiple line comments
+	I am a multiple line comments
+'''
+print('end comments')
