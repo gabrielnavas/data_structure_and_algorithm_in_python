eb79bd3172bc8d780de1edcb83649522a2287651
Navas <gabrielnavas@email.com>
2021-03-17 18:54:50 -0300

    basics: primitives

---
+++ basic/primitives.py
@@ -0,0 +1,13 @@
+"""
+  Type datas:
+
+  str - string
+  int - integer
+  float - real
+  bool - boolean/logic
+"""
+
+print(type('Gabriel')) # <class 'str'>
+print(type(123)) # <class 'int'>
+print(type(123.01)) # <class 'float'>
+print(type(10 == 10)) # <class 'bool'>
