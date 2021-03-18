d90b269438eeed1a0e9602186e963a62a5ac1335
Navas <gabrielnavas@email.com>
2021-03-18 18:43:37 -0300

    inputs

---
+++ basic/exame_01.py
@@ -0,0 +1,15 @@
+name = 'Gabriel'
+age = 28
+height = 1.85
+weight = 75
+year_now = 2021
+year_born = year_now - age
+imc = weight / (height**2)
+
+print(f'name: {name}')
+print(f'age: {age}')
+print(f'height: {height}')
+print(f'weight: {weight}')
+print(f'year now: {year_now}')
+print(f'year born: {year_born}')
+print(f'imc: {imc:.2f}')
---
+++ basic/input.py
@@ -0,0 +1,7 @@
+my_name = input('What is your name? ')
+my_age = input('What is your age? ')
+
+year_now = 2021
+born = year_now - int(my_age)
+
+print(f'my name is {my_name} and born in {born}')
