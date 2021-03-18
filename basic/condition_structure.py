ba4d524997a69905a4cef343f7ae480ee6716f42
Navas <gabrielnavas@email.com>
2021-03-18 19:34:19 -0300

    condition_structure

---
+++ basic/condition_structure.py
@@ -0,0 +1,16 @@
+name = 'Gabriel'
+
+if name == 'Gabriel':
+  print('Olá')
+elif name == 'gabriel': 
+  print('bye')
+
+if name in ['gabriel', 'gab', 'Gabriel']:
+  print('i am...')
+
+if False:
+  print('ola')
+elif True:
+  print('hellooww gabb')
+else:
+  print('olaa')
---
+++ basic/operations.py
@@ -0,0 +1,34 @@
+if 2 > 1:
+  print('1 > 2: True')
+
+if 5 < 10:
+  print('5 < 10: True')
+
+
+if 10 == 10:
+  print('10 is equals 10')
+
+if 5 >= 2 and 2 <= 55:
+  print('5 >= 2 and 2 <= 55: True')
+
+if 2 <= 5:
+  print('2 <= 5: True')
+  
+ten = 10
+if 5 < ten < 20:
+  print('ten is > 5 and < 20')
+
+
+if not 1 > 2:
+  print('1 is < 2') 
+
+
+if 1 > 2 or 10 == 10:
+  print('1 is > 2 or 10 = 10')
+
+
+if 'gab' in 'gabriel':
+  print('gabriel contains gab')
+
+if not 'navas' in 'gabriel':
+  print('navas not in gabriel')
