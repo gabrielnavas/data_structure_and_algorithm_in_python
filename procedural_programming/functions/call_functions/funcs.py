164864ad6cd533059d0091f9f25f6add4c8de5dd
Navas <gabrielnavas@email.com>
2021-05-25 21:25:27 -0300

    call functions

---
+++ procedural_programming/functions/call_functions/funcs.py
@@ -0,0 +1,5 @@
+def names():
+    return ['Gabriel', 'Anna', 1, 2]
+
+def whois(name):
+    return f'Who is {name}'    
---
+++ procedural_programming/functions/call_functions/main.py
@@ -0,0 +1,7 @@
+from funcs import names, whois
+
+nms = names()
+print(nms)
+
+who = whois('gabriel')
+print(who)
